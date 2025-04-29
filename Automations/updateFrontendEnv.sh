4fdbecb2e050ec5475b9a0a765d9f588df228305
test <149031026+p-k-pavan@users.noreply.github.com>
2025-04-29 11:04:49 +0530

    Update updateFrontendEnv.sh

--- Automations/updateFrontendEnv.sh
+++ Automations/updateFrontendEnv.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-INSTANCE_ID=i-0ce3b250da2fe8079
+# Set the Instance ID and path to the .env file
+INSTANCE_ID="i-0ce3b250da2fe8079"
 
-# Get public IP address of the instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Retrieve the public IP address of the specified EC2 instance
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
+# Path to the .env file
 file_to_find="../frontend/.env"
 
-# Check if file exists
-if [ ! -f "$file_to_find" ]; then
-    echo "ERROR: File not found."
-    exit 1
-fi
-
-# Read the content of the .env file
-current_url=$(cat "$file_to_find")
+# Check the current FRONTEND_URL in the .env file
+current_url=$(sed -n "4p" $file_to_find)
 
 # Update the .env file if the IP address has changed
-if [[ "$current_url" != *"VITE_BASE_URL=\"http://${ipv4_address}:8888\""* ]]; then
-    sed -i -e "s|^VITE_BASE_URL=.*|VITE_BASE_URL=\"http://${ipv4_address}:8888\"|g" "$file_to_find"
-    echo "BASE_URL updated to http://${ipv4_address}:8888"
-else
-    echo "BASE_URL already up-to-date."
+if [[ "$current_url" != "VITE_BASE_URL=\"http://${ipv4_address}:8888\"" ]]; then
+    if [ -f $file_to_find ]; then
+        sed -i -e "s|VITE_BASE_URL.*|VITE_BASE_URL=\"http://${ipv4_address}:8888\"|g" $file_to_find
+    else
+        echo "ERROR: File not found."
+    fi
 fi
